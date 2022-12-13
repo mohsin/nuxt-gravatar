432a96f57457b6aa068f269d9c952c105a0286e7
Saifur Rahman Mohsin <mohsin92@me.com>
2022-12-13 20:12:47 +0530

    test: Switch coverage reporting to use coveralls

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
             inline: ['@nuxt/test-utils-edge'],
         },
         coverage: {
-            reporter: ['text', 'html'],
+            reporter: ['html', 'lcov'],
         }
     },
 })
